6f1add2317b4fd3addb16fd0169841cd8929bf09
OskarKozaczka <oskaro0@vp.pl>
2022-04-14 22:08:17 +0200

    .

--- CaloriesTracker/src/App.vue
+++ CaloriesTracker/src/App.vue
@@ -4,12 +4,14 @@ import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
 <template>
+  <div>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/history">History</RouterLink>
         <RouterLink to="/settings">Settings</RouterLink>
       </nav>
-  <RouterView />
+    <RouterView />
+  </div>
 </template>
 
 <style>
--- CaloriesTracker/src/views/HomeView.vue
+++ CaloriesTracker/src/views/HomeView.vue
@@ -1,9 +1,69 @@
 <script setup>
 import TheWelcome from '@/components/TheWelcome.vue'
+import $ from "jquery";
+
+	$(document).ready(function() {
+			$('.minus').click(function () {
+				var $input = $(this).parent().find('input');
+				var count = parseInt($input.val()) - 1;
+				count = count < 1 ? 1 : count;
+				$input.val(count);
+				$input.change();
+				return false;
+			});
+			$('.plus').click(function () {
+				var $input = $(this).parent().find('input');
+				$input.val(parseInt($input.val()) + 1);
+				$input.change();
+				return false;
+			});
+		});
 </script>
 
 <template>
   <main>
+      <div class="number">
+        <span class="minus">-</span>
+        <input type="text" value="1"/>
+        <span class="plus">+</span>
+      </div>
+      <img src="@/components/icons/Pizza_Pepperoni.svg">
+
     <TheWelcome />
   </main>
 </template>
+<style>
+img {
+  display: flex;
+  justify-content: center;
+  margin: auto;
+}
+	span {cursor:pointer; }
+		.number{
+			margin:10px;
+      display: flex;
+      justify-content: center;
+		}
+		.minus, .plus{
+			width:20px;
+			height:20px;
+			background:#f2f2f2;
+			border-radius:4px;
+			padding:8px 5px 8px 5px;
+			border:1px solid #ddd;
+      display: inline-block;
+      vertical-align: middle;
+      text-align: center;
+		}
+		input{
+			height:34px;
+      width: 100px;
+      text-align: center;
+      font-size: 26px;
+			border:1px solid #ddd;
+			border-radius:4px;
+      display: inline-block;
+      vertical-align: middle;
+    }
+
+</style>
