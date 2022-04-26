648e38debac135f9f7303f7d1eb242ab9d1e202c
OskarKozaczka <oskaro0@vp.pl>
2022-04-27 01:02:03 +0200

    push test

--- public/sw.js
+++ public/sw.js
@@ -2,4 +2,26 @@ import { precacheAndRoute } from 'workbox-precaching'
   
 self.addEventListener('push', event => {console.log("push")})
 
+self.addEventListener('push', function(event) {
+    console.log('Received a push message', event);
+
+    var title = 'Yay a message.';
+    var body = 'We have received a push message.';
+    var icon = '/images/icon-192x192.png';
+    var tag = 'simple-push-demo-notification-tag';
+    var data = {
+    doge: {
+        wow: 'such amaze notification data'
+    }
+    };
+
+    event.waitUntil(
+    self.registration.showNotification(title, {
+        body: body,
+        icon: icon,
+        tag: tag,
+        data: data
+    })
+    );
+});
 precacheAndRoute(self.__WB_MANIFEST)
