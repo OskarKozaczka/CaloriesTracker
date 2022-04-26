a8cf99e7f3ac1b22129a9a16f39c481ea15e19dc
OskarKozaczka <oskaro0@vp.pl>
2022-04-27 00:40:34 +0200

    push

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -20,7 +20,21 @@ import { ref, onMounted,createApp } from 'vue'
 			});
 		});
 
-		
+		navigator.serviceWorker.register('sw.js');
+
+
+			Notification.requestPermission(function(result) {
+				if (result === 'granted') {
+				navigator.serviceWorker.ready.then(function(registration) {
+					registration.showNotification('Vibration Sample', {
+					body: 'Buzz! Buzz!',
+					icon: '../images/touch/chrome-touch-icon-192x192.png',
+					vibrate: [200, 100, 200, 100, 200, 100, 200],
+					tag: 'vibration-sample'
+					});
+				});
+				}
+			});
 </script>
 <script>
 
