3cbcdc08785a212493f83de3c269fdc12277ce60
OskarKozaczka <oskaro0@vp.pl>
2022-04-26 23:20:49 +0200

    pwa

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './registerServiceWorker'
+import { registerSW } from 'virtual:pwa-register'
 
-
-
+const updateSW = registerSW({
+    onOfflineReady() {},
+  })
 
 const app = createApp(App)
 
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,37 @@
 import { fileURLToPath, URL } from 'url'
-
+import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],  
+      manifest: {
+        name: 'Calories Tracker',
+        short_name: 'Calories Tracker',
+        description: 'Track your Calories!',
+        display: "standalone",
+        theme_color: '#ffffff',
+        start_url: '/',
+        scope: "/",
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          }
+        ]
+      }
+    })
+  ]   ,
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
