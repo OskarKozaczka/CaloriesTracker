571993e592696efb0dc2e59c36b9bbce3e0bd4d3
mbruzda <marcin.bru@op.pl>
2022-04-27 14:54:44 +0200

    added notifications

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -49,6 +49,14 @@ export default {
             <button type="submit" @click="updateTarget"> Zmień </button>
         </div>
 
+        <div class="notify">
+            <label class="switch">
+            <input type="checkbox">
+            <span class="slider round"></span> 
+            </label>
+            <div class="label">Powiadomienia</div>
+        </div>
+
         <button class="logout" @click="signOut"> Wyloguj </button>
         <button class="delete" @click="deleteUser"> Usuń konto </button>
   </div>
@@ -60,6 +68,9 @@ export default {
     font-family: "Roboto";
     display: flex;
     flex-direction: column;
+}
+.notify{
+    margin: 15px
 }
 
 .title {
@@ -121,5 +132,68 @@ button {
     margin: auto;
     margin-top: 10px;
     width: 60%;
+}
+
+/* The switch - the box around the slider */
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #2196F3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
 }
 </style>
